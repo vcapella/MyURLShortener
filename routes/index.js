d872309103188aa822e85144e940d45b52f9a497
Victor Capella <vesc@Macs-MacBook-Pro.local>
2022-02-16 17:48:14 -0500

    create routes and randomize

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,26 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Shortener" });
+});
+
+function generateCode() {
+  let text = "";
+  const possible =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  for (let i = 0; i < 7; i++)
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  return text;
+}
+
+router.post("/new", (req, res, next) => {
+  const url = req.body.url;
+  console.log(url);
+
+  const code = generateCode();
+  res.send("http://localhost:3000/" + code);
 });
 
 module.exports = router;
